007162c8e2c4ff0537abe4312c770c6acdbc0a6f
Saarule <saaryazdi@gmail.com>
2021-04-21 17:53:20 +0300

    Dapps

--- components/screens/HomeScreen.js
+++ components/screens/HomeScreen.js
@@ -4,6 +4,7 @@ import styled from "styled-components";
 
 import Text from "../Text";
 import categoryList from "../../categories";
+import dapps from "../../dapps";
 
 export default HomeScreen = () => {
 
@@ -12,6 +13,14 @@ export default HomeScreen = () => {
     const changeCategory = (category) => {
         setSelectedCategory(category);
     };
+
+    const DappItem = (dapp) => {
+        return (
+            <Dapp>
+                <DappCover source={dapp.cover} />
+            </Dapp>
+        )
+    }
 
     return (
         <Container>
@@ -35,11 +44,16 @@ export default HomeScreen = () => {
                 {categoryList.map((category, index) => {
                     return (
                         <Category key={index} onPress={() => changeCategory(category)}>
-                            <CategoryName selected={selectedCategory === category ? true : false}>{category}</CategoryName>
+                            <CategoryName selected={selectedCategory === category ? true : false}>
+                                {category}
+                            </CategoryName>
+                            {selectedCategory === category && <CategoryDot />}
                         </Category>
                     )
                 })}
             </Categories>
+
+            <Dapps data={dapps} KeyExtractor={item => String(item.id)} renderItem={({ item }) => DappItem(item)} />
         </Container>
     );
 };
@@ -76,3 +90,24 @@ const CategoryName = styled(Text)`
     color: ${(props) => (props.selected ? "#819ee5" : "#9a9a9a")};
     font-weight: ${(props) => (props.selected ? "700" : "500")};
     `;
+
+const CategoryDot = styled.View`
+    width: 6px;
+    height: 6px;
+    border-radius: 3px;
+    background-color: #819ee5;
+`;
+
+const Dapps = styled.FlatList`
+    margin-top: 32px;
+    flex: 1;
+`;
+
+const Dapp = styled.TouchableOpacity`
+    margin-bottom: 32px;
+`;
+
+const DappCover = styled.Image`
+    height: 300px;
+    width: 100%;
+`;
---
+++ dapps.js
@@ -0,0 +1,50 @@
+export default dapps = [
+    {
+        id: 1,
+        title: "Uniswap",
+        teaser: "Best DEX on Ethereum blockchain",
+        description: "Swap tokens on a decentralized manner",
+        rating: 4.5,
+        category: ["Top", "DEXs"],
+        age: "12+",
+        cover: require("./assets/uniswap-cover.jpg"),
+        screenshots: [
+            require("./assets/uniswap-screenshot1.png"),
+            require("./assets/uniswap-screenshot1.png"),
+            require("./assets/uniswap-screenshot1.png"),
+        ],
+        backgroundColor: "#824571"
+    },
+    {
+        id: 2,
+        title: "Compound",
+        teaser: "Compound Finance",
+        description: "Earn intrest and borrow with your assests",
+        rating: 4.5,
+        category: ["Top", "Lending/Borrowing"],
+        age: "12+",
+        cover: require("./assets/compound-cover.png"),
+        screenshots: [
+            require("./assets/compound-screenshot1.png"),
+            require("./assets/compound-screenshot1.png"),
+            require("./assets/compound-screenshot1.png"),
+        ],
+        backgroundColor: "#824571"
+    },
+    {
+        id: 3,
+        title: "1inch",
+        teaser: "1inch Aggregator",
+        description: "Top DEXs aggregator",
+        rating: 4.5,
+        category: ["Top", "DEXs", "Aggregators"],
+        age: "12+",
+        cover: require("./assets/1inch-cover.png"),
+        screenshots: [
+            require("./assets/1inch-screenshot1.jpg"),
+            require("./assets/1inch-screenshot1.jpg"),
+            require("./assets/1inch-screenshot1.jpg"),
+        ],
+        backgroundColor: "#824571"
+    },
+]
